bb5e98f0f69354b52e3d4cbaed10e9fc0cbec1cc
clement code <mrdominiqueclmeent@gmail.com>
2022-06-14 18:31:03 +0200

    seconde assocation clement-francois

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UsersModule } from './users/users.module';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { FiltersComponent } from './filters/filters.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HotelsComponent } from './pages/hotels/hotels.component';
+import { ListHotelsComponent } from './list-hotels/list-hotels.component';
+import { ResearchBarComponent } from './research-bar/research-bar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ResearchBarComponent,
+    HeaderComponent,
+    HomeComponent,
+    DatepickerComponent,
+    FooterComponent,
+    ListHotelsComponent,
+    FiltersComponent,
+    HotelsComponent,
 
 
   ],
@@ -24,6 +40,7 @@ import { UsersModule } from './users/users.module';
     HttpClientModule,
     BrowserAnimationsModule,
     UsersModule,
+
 
   ],
   providers: [],
