7070c3137f7c6ec9c7b766937183f05ce9d3694c
clement code <mrdominiqueclmeent@gmail.com>
2022-06-16 15:11:13 +0200

    creation module facturation + css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { PagesModule } from './pages/pages.module';
-
 import { UsersModule } from './users/users.module';
-import { CommandeComponent } from './facturation/commande/commande.component';
+import { FacturationModule } from './facturation/facturation.module';
 
 
 
@@ -18,7 +16,7 @@ import { CommandeComponent } from './facturation/commande/commande.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CommandeComponent,
+
 
 
   ],
@@ -31,6 +29,7 @@ import { CommandeComponent } from './facturation/commande/commande.component';
     UsersModule,
     CoreModule,
     PagesModule,
+    FacturationModule,
 
   ],
   providers: [],
