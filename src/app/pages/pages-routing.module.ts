306a3e8e2b941859004ce120707f2c91f7906f52
clement code <mrdominiqueclmeent@gmail.com>
2022-06-16 14:57:40 +0200

    rooting update user facturation

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from '../facturation/cart/cart.component';
+import { CommandeComponent } from '../facturation/commande/commande.component';
+import { FindallutilisateurComponent } from '../users/findallutilisateur/findallutilisateur.component';
+import { InscriptionComponent } from '../users/inscription/inscription.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'utilisateurs', component: FindallutilisateurComponent },
+  { path: '', component: HomeComponent },
+  { path: 'panier', component: CartComponent },
+  { path: 'user/commande', component: CommandeComponent },
 ];
 
 @NgModule({
