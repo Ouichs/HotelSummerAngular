8b78bfc8c8cbbd7b4f144193b0a2c3d76af64a73
Nicolas Endredi <nicolasendredi@gmail.com>
2022-06-17 10:28:49 +0200

    Afficher déconnexion si l'utilisateur est connecte

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ApiAuthService } from 'src/app/auth/api-auth.service';
 import { DataServiceService } from 'src/app/users/dataService.service';
 
 @Component({
@@ -14,14 +15,14 @@ import { DataServiceService } from 'src/app/users/dataService.service';
 
 export class HeaderComponent implements OnInit {
 
-
+  loggedin:boolean
   isAvailable = false;
   isAvailable$: Observable<boolean>
   utilsateur: string
-  constructor(public route: Router, public data: DataServiceService) { }
+  constructor(public route: Router, public data: DataServiceService, private serv:ApiAuthService) { }
 
   ngOnInit(): void {
-
+    this.loggedin = this.serv.isLoggedIn
     this.utilsateur = this.data.connection
 
 
@@ -34,6 +35,10 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-
+  logout() {
+    this.serv.doLogout()
+    this.loggedin = this.serv.isLoggedIn
+    this.ngOnInit()
+  }
 
 }
